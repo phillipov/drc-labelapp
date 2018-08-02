25266256788d80df46beaa4bdcbed24acd2950f4
phillipov <phillipov@outlook.com>
2018-08-02 12:44:17 -0700

    Last commit before removing CSVHelper and creating new CSV data class in the model package.

--- src/labelapp/model/CSVHelper.java
+++ src/labelapp/model/CSVHelper.java
@@ -9,13 +9,46 @@ import java.util.List;
 
 public class CSVHelper {
     public static void main(String[] args) {
-        try {
-            CSVReader reader = new CSVReader(new FileReader("yourfile.csv"));
-            List<String[]> myEntries = reader.readAll();
+        JFrame f = new JFrame();
+        f.add(getDataTable("src/export.csv"));
+        f.pack();
+        f.setVisible(true);
+    }
 
-            JTable table = new JTable(myEntries.toArray()); //add list to 2d array method
+    public static JTable getDataTable(String fileName) {
+        JTable table = new JTable();
+        try {
+            CSVReader reader = new CSVReader(new FileReader(fileName));
+            List<String[]> entries = reader.readAll();
+
+            String[][] data = makeDataArray(entries);
+            String[] columns = data[0];
+            data = removeDataHeader(data);
+
+            table = new JTable(data, columns); //add list to 2d array method
         } catch(IOException ioe) {
             ioe.printStackTrace();
         }
+        return table;
+    }
+
+    private static String[][] makeDataArray(List<String[]> dataList) {
+        String[][] dataArray = new String[dataList.size()][dataList.get(0).length];
+
+        for(int i = 0; i < dataList.size(); i++) {
+            dataArray[i] = dataList.get(i);
+        }
+
+        return dataArray;
+    }
+
+    private static String[][] removeDataHeader(String[][] oldData) {
+        String[][] newData = new String[oldData.length - 1][oldData[0].length];
+
+        for(int i = 0; i < oldData.length; i++) {
+            newData[i] = oldData[i + 1];
+        }
+
+        return newData;
     }
 }
