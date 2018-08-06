c78262ebf736a3815eef8065494f6dcd6130d3dd
phillipov <phillipov@outlook.com>
2018-08-06 16:55:36 -0700

    Making a new CSV data object to store data as a list of items.

---
+++ src/labelapp/model/CSVItems.java
@@ -0,0 +1,11 @@
+package labelapp.model;
+
+import java.io.File;
+
+public class CSVItems extends CSVData {
+
+    // TODO: 8/6/2018 Write this as a CSVData object that represents its data as a list of items, not an array
+    public CSVItems(File file) {
+        super(file);
+    }
+}
